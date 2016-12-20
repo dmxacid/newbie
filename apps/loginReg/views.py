09b55845b9e7759f9ea7c403ba2daf8d909c65d4
Steven Truong <steventruongm@gmail.com>
2016-12-19 17:25:19 -0800

    initla commit

---
+++ apps/loginReg/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    return render(request, 'loginReg/index.html')
+def register(request):
+    viewsResponse = User.objects.add_user(request.POST)
+    if viewsResponse['isRegistered']:
+        request.session['user_id'] = viewsResponse['user'].id
+        request.session['user_fname'] = viewsResponse['user'].first_name
+        return redirect('users:success')
+    else:
+        for error in viewsResponse['errors']:
+            messages.error(request, error)
+        return redirect('users:index')
+def success(request):
+    return render(request, 'loginReg/success.html')
+def login(request):
+    viewsResponse = User.objects.login_user(request.POST)
+    if viewsResponse['isLoggedIn']:
+        request.session['user_id'] = viewsResponse['user'].id
+        request.session['user_fname'] = viewsResponse['user'].first_name
+        return redirect('users:success')
+    else:
+        for error in viewsResponse['errors']:
+            messages.error(request, error)
+        return redirect('users:index')
+def logout(request):
+    request.session.clear()
+    return redirect('users:index')
